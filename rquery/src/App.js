7770eeb7b368f21f21a619edcee3ce898bbb763e
Valery <chinvalery@gmail.com>
2021-06-30 20:01:04 +0200

    [scroll]add data on scroll, update data without using a reload page

--- rquery/src/App.js
+++ rquery/src/App.js
@@ -1,26 +1,67 @@
-import { useQuery, isError } from 'react-query'
+import { Fragment, useRef } from 'react';
+import { useQuery, useInfiniteQuery } from 'react-query'
+import useIntersectionObserver from "./hook/useIntersectionObserver"
 import './App.css';
 
 const fetchUsers = async () => {
-  const res = await fetch("https://reqres.in/api/users?per_page=12")
+  const res = await fetch(`https://reqres.in/api/users?{pageParam}&per_page=3`)
   return res.json()
 }
 
 function App() {
-  const { isLoading, data, isError } = useQuery("users", fetchUsers)
+  const {
+    data,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery('users', fetchUsers, {
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.page < lastPage.total_pages) return lastPage.page + 1
+    }
+  })
+
+  const loadMoreButtonRef = useRef()
+
+  useIntersectionObserver({
+    target: loadMoreButtonRef,
+    onIntersect: fetchNextPage,
+    enabled: hasNextPage,
+  })
+
+
   console.log(data)
 
   if (isLoading) return (<div className="App">Loading...</div>)
   if (isError) return (<div className="App">Erreur</div>)
   return (<div className="App">
     <h2>List of Users </h2>
-    <ul>
-      {data.data.map(user => (
-        <li key={user.id}>
-          {user.first_name} {user.last_name}
-        </li>
-      ))}
-    </ul>
+
+    {data.pages.map((group, i) => (
+
+      <Fragment key={i}>
+        {group.data.map(user =>
+          <div key={user.id} className="list">
+            {user.first_name} {user.last_name}
+          </div>
+        )}
+      </Fragment>
+    ))}
+    <button
+      ref={loadMoreButtonRef}
+      onClick={() => fetchNextPage()}
+      disabled={!hasNextPage || isFetchingNextPage}
+    >
+      {isFetchingNextPage
+        ? 'Loading more...'
+        : hasNextPage
+          ? 'Load More'
+          : 'Nothing more to load'}
+    </button>
+    <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
   </div>)
 }
 
